2af56393c9561d98da7ba0f7a87519c75eb65e23
gulayky <ghillez@gmail.com>
2020-02-29 15:23:57 -0500

    firstcommit

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,5 +4,8 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("great person Furkan");
+        String str = "Java is Fun!";
+        System.out.println(str);
     }
 }
